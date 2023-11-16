f40d4ab1c39d4593686208fe98e0ab4e0f362282
YunShu <im@yunshu.site>
2023-11-16 22:10:55 +0800

    chore: 更新依赖

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/app';
-import './app.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app";
+import "./app.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
